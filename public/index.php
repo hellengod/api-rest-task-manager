0c8dae6bf5b619e7a3e0a18c549dbe495ce969a0
Hellen Bianchini <hellenbiangod@gmail.com>
2025-08-27 11:40:42 -0300

    feat: adiciona roteamento

--- public/index.php
+++ public/index.php
@@ -3,6 +3,27 @@
 require __DIR__ . '/../vendor/autoload.php';
 use App\Controller\TaskController;
 
-if($_SERVER['REQUEST_METHOD']=='POST' && $_SERVER['REQUEST_URI']=='/tasks'){
+if ($_SERVER['REQUEST_METHOD'] == 'POST' && $_SERVER['REQUEST_URI'] == '/tasks') {
     TaskController::create();
+} else if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+    $parts = explode('/', $_SERVER['REQUEST_URI']);
+
+    if ($parts[1] === 'tasks' && !isset($parts[2])) {
+        TaskController::get();
+    } elseif ($parts[1] === 'tasks' && isset($parts[2])) {
+        $id = $parts[2];
+        TaskController::getTaskById($id);
+    }
+} else if ($_SERVER['REQUEST_METHOD'] == 'PUT') {
+    $parts = explode('/', $_SERVER['REQUEST_URI']);
+    if ($parts[1] === 'tasks' && isset($parts[2])) {
+        $id = $parts[2];
+        TaskController::update($id);
+    }
+} else if ($_SERVER['REQUEST_METHOD'] == 'DELETE') {
+    $parts = explode('/', $_SERVER['REQUEST_URI']);
+    if ($parts[1] === 'tasks' && isset($parts[2])) {
+        $id = $parts[2];
+        TaskController::delete($id);
+    }
 }
